f0f06ca0428003f3e81cad77562f5bc538e916bf
shi00317 <shi00317@umn.edu>
2023-02-04 14:01:49 -0600

    create bash code for docker naoqi sdk setup

---
+++ naoqi_sdk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://community-static.aldebaran.com/resources/2.1.4.13/sdk-python/pynaoqi-python2.7-2.1.4.13-linux64.tar.gz
+
+tar xzf pynaoqi-python2.7-2.1.4.13-linux64.tar.gz
+
+rm pynaoqi-python2.7-2.1.4.13-linux64.tar.gz
